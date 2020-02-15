06152d61f11f6044ce2f121edb465c8f559be872
Karlo Marinovic <36795522+ikaem@users.noreply.github.com>
2020-02-15 09:29:55 +0100

    first and final commit for now

---
+++ src/fixtures/index.js
@@ -0,0 +1,17 @@
+export const singularStory = {
+    by: "Karlo Marinović",
+    id: 1,
+    time: 1581699271,
+    title: "No Worries something",
+    url: "https://something.com",
+};
+
+export const storyIds = [1];
+
+export const emptySingularStory = {
+    by: undefined,
+    id: undefined,
+    time: undefined,
+    title: undefined,
+    url: undefined,
+}
